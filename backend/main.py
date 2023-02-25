de5d1bc8300f93cca9ba5f91e9be43990a6ec604
陈子扬 <ziyang@s-MacBook-Pro.local>
2023-02-25 23:46:21 +0000

    refactor

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from typing import Union
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 from fastapi import File, UploadFile
@@ -63,14 +63,7 @@ async def create_upload_file(files: List[UploadFile] = File(...)):
     # TODO: should we keep "1.", "2."... and "Answer:" in the strings
     # q/a/q/a...
     split_string = answer.split('\n')
-    """
-    json_response = [{'q0': split_string[0], 'a0': split_string[1],
-                      'q1': split_string[2], 'a1': split_string[3],
-                      'q2': split_string[4], 'a2': split_string[5],
-                      'q3': split_string[6], 'a3': split_string[7],
-                      'q4': split_string[8], 'a4': split_string[9]}]
-    """
-    json_response = [{'q0': split_string[0], # One \n between q and a, two between a and next q 
+    json_response = [{'q0': split_string[0], # One \n between q and a, two between a and next q
                           'q1': split_string[3],
                           'q2': split_string[6],
                           'q3': split_string[9],
@@ -87,38 +80,6 @@ async def create_upload_file(files: List[UploadFile] = File(...)):
     #     {"Success": "true"}]
     # json_compatible_item_data = jsonable_encoder(dummy_response)
     return JSONResponse(content=json_compatible_item_data)
-
-
-@app.get("/test")
-def ocr_gpt():
-    with open('context.pickle', 'rb') as handle:
-        context = pickle.load(handle)
-        answer = execute(context, id=0, age="university student", prompt="")
-        # Formatting as JSON
-        # TODO: should we keep "1.", "2."... and "Answer:" in the strings
-        # q/a/q/a...
-        split_string = answer.split('\n')
-        print(split_string)
-
-        """
-        json_response = [{'q0': split_string[0], 'a0': split_string[1],
-                          'q1': split_string[2], 'a1': split_string[3],
-                          'q2': split_string[4], 'a2': split_string[5],
-                          'q3': split_string[6], 'a3': split_string[7],
-                          'q4': split_string[8], 'a4': split_string[9]}]
-        """
-        json_response = [{'q0': split_string[0], # One \n between q and a, two between a and next q 
-                          'q1': split_string[3],
-                          'q2': split_string[6],
-                          'q3': split_string[9],
-                          'q4': split_string[12],
-                          'q5': split_string[15],
-                          'q6': split_string[18],
-                          'q7': split_string[21],
-                          'q8': split_string[24],
-                          'q9': split_string[27]}]
-        json_compatible_item_data = jsonable_encoder(json_response)
-        return JSONResponse(content=json_compatible_item_data)
 
 """
 @app.get('/check_answers/{q0}/{a0}/{q1}/{a1}/{q2}/{a2}/{q3}/{a3}/{q4}/{a4}')
@@ -138,8 +99,9 @@ def giving_back_score(q0: str, a0: str, q1: str, a1: str, q2: str, a2: str, q3: 
         return JSONResponse(content=json_compatible_item_data)
 """
 
-@app.get('/check_answers/{questions_answers}')
-def giving_back_score(questions_answers):
+@app.post('/check_answers/')
+def giving_back_score(request: Request):
+    questions_answers = request.json()
     # Format them back to a single string for GPT
     string_input = '1. '+questions_answers[0]['question']+'\n'+'Answer: '+questions_answers[0]['answer']+'\n'+'\n'+'2. '+questions_answers[1]['question']+'\n'+'Answer: '+questions_answers[1]['answer']+'\n'+'\n'+'3. '+questions_answers[2]['question']+'\n'+'Answer: '+questions_answers[2]['answer']+'\n'+'\n'+'4. '+questions_answers[3]['question']+'\n'+'Answer: '+questions_answers[3]['answer']+'\n'+'\n'+'5. '+questions_answers[4]['question']+'\n'+'Answer: '+questions_answers[4]['answer']+'\n'+'\n'+'6. '+questions_answers[5]['question']+'\n'+'Answer: '+questions_answers[5]['answer']+'\n'+'\n'+'7. '+questions_answers[6]['question']+'\n'+'Answer: '+questions_answers[6]['answer']+'\n'+'\n'+'8. '+questions_answers[7]['question']+'\n'+'Answer: '+questions_answers[7]['answer']+'\n'+'\n'+'9. '+questions_answers[8]['question']+'\n'+'Answer: '+questions_answers[8]['answer']+'\n'+'\n'+'10. '+questions_answers[9]['question']+'\n'+'Answer: '+questions_answers[9]['answer']
     with open('context.pickle', 'rb') as handle:
