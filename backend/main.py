104886f130cf088222fcb593bd25cfb9cf8bff3a
Niladri Shekhar Dutt <niladrishekhardutt@gmail.com>
2023-02-25 19:59:00 +0000

    multiple files

--- backend/main.py
+++ backend/main.py
@@ -6,6 +6,7 @@ from fastapi.responses import JSONResponse
 from fastapi import File, UploadFile
 from pydantic import BaseModel
 import easyocr
+from typing import List
 import numpy as np
 import shutil
 import os  # To be able to mkdir
@@ -39,102 +40,23 @@ def read_root():
     return {"Hello": "World"}
 
 
-""" Below are the examples from the FastAPI documentation, they work.
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
-"""
-
-
-# Create our methods
-
-## CREATION
-# Creating a folder for a new subject
-@app.get("/create/{subject_id}")
-def create_folder(subject_id: str):
-    dirname = os.path.dirname(__file__)  # Retrieving the current dir path
-    filename = os.path.join(dirname, name_of_the_subfolder,
-                            subject_id)  # Creating the new path
-    os.mkdir(filename)
-    return  # TBD
-
-
-# Creating a folder for a new week
-@app.get("/create/{subject_id}/{week_id}")
-def create_folder(subject_id: str, week_id: int):
-    dirname = os.path.dirname(__file__)  # Retrieving the current dir path
-    filename = os.path.join(
-        dirname, name_of_the_subfolder, subject_id, week_id)
-    os.mkdir(filename)
-    return  # TBD
-
-
-## DELETION
-# Either for a subject
-@app.delete("/delete/{subject_id}")
-def create_folder(subject_id: str):
-    dirname = os.path.dirname(__file__)  # Retrieving the current dir path
-    filename = os.path.join(dirname, name_of_the_subfolder,
-                            subject_id)  # Creating the new path
-    shutil.rmtree(filename, ignore_errors=False)
-    return  # TBD
-
-
-# Or for a week
-@app.delete("/delete/{subject_id}/{week_id}")
-def create_folder(subject_id: str, week_id: int):
-    dirname = os.path.dirname(__file__)  # Retrieving the current dir path
-    filename = os.path.join(
-        dirname, name_of_the_subfolder, subject_id, week_id)
-    shutil.rmtree(filename, ignore_errors=False)
-    return  # TBD
-
-
-# Or for a specific file
-@app.delete("/delete/{subject_id}/{week_id}/{file_id}")
-def create_folder(subject_id: str, week_id: int, file_id: str):
-    dirname = os.path.dirname(__file__)  # Retrieving the current dir path
-    filename = os.path.join(
-        dirname, name_of_the_subfolder, subject_id, week_id, file_id)
-    os.remove(filename)
-    return  # TBD
-
-
-## UPLOADING
-# Can only upload files
-@app.post("/upload/{subject_id}/{week_id}/{file_id}")
-def create_folder(subject_id: str, week_id: int, file_id: str):
-    dirname = os.path.dirname(__file__)  # Retrieving the current dir path
-    filename = os.path.join(
-        dirname, name_of_the_subfolder, subject_id, week_id, file_id)
-    """
-    TBD
-    """
-    return  # TBD
-
-
-## DASHBOARD
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile):
-    with open("temp", 'wb+') as writer:
-        shutil.copyfileobj(file.file, writer)
-        result = reader.readtext("temp")
-        ocr_result = ""
-        for i in result:
-            ocr_result += (i[1] + " ")
-        df = text_to_df(ocr_result)
-        context = get_context_encoding(df)
-        with open('context.pickle', 'wb') as handle:
-            pickle.dump(context, handle, protocol=pickle.HIGHEST_PROTOCOL)
-        dummy_response = [
-            {"Success": "true"}]
-        json_compatible_item_data = jsonable_encoder(dummy_response)
-        return JSONResponse(content=json_compatible_item_data)
+@app.post("/uploadfiles/")
+async def create_upload_file(files: List[UploadFile] = File(...)):
+    ocr_result = ""
+    for file in files:
+        with open(file.filename, 'wb+') as writer:
+            shutil.copyfileobj(file.file, writer)
+            result = reader.readtext(file.filename)
+            for i in result:
+                ocr_result += (i[1] + " ")
+    df = text_to_df(ocr_result)
+    context = get_context_encoding(df)
+    with open('context.pickle', 'wb') as handle:
+        pickle.dump(context, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    dummy_response = [
+        {"Success": "true"}]
+    json_compatible_item_data = jsonable_encoder(dummy_response)
+    return JSONResponse(content=json_compatible_item_data)
 
 
 @app.get("/test")
@@ -146,8 +68,11 @@ def ocr_gpt():
         # TODO: should we keep "1.", "2."... and "Answer:" in the strings
         # q/a/q/a...
         split_string = answer.split('\n')
-        json_response = [{'q0':split_string[0], 'a0':split_string[1], 'q1':split_string[2], 'a1':split_string[3], 'q2':split_string[4], 'a2':split_string[5], 'q3':split_string[6], 'a3':split_string[7], 'q4':split_string[8], 'a4':split_string[9]}]
+        json_response = [{'q0': split_string[0], 'a0': split_string[1],
+                          'q1': split_string[2], 'a1': split_string[3],
+                          'q2': split_string[4], 'a2': split_string[5],
+                          'q3': split_string[6], 'a3': split_string[7],
+                          'q4': split_string[8], 'a4': split_string[9]}]
 
-        json_compatible_item_data = jsonable_encoder(json_response)  
+        json_compatible_item_data = jsonable_encoder(json_response)
         return JSONResponse(content=json_compatible_item_data)
-
