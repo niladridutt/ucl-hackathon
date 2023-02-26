ed12f82c97cb78c57f22269850463281180691ee
andreibunea99 <56512139+andreibunea99@users.noreply.github.com>
2023-02-26 10:47:42 +0000

    again

--- frontend/frontend/src/app/app-routing.module.ts
+++ frontend/frontend/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { QuizComponent } from './quiz/quiz.component';
 import { UploadFileComponent } from './upload-file/upload-file.component';
 
 const routes: Routes = [
   { path: '', component: HomepageComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'upload-file', component: UploadFileComponent }
+  { path: 'upload-file', component: UploadFileComponent },
+  { path: 'quiz', component: QuizComponent }
 ];
 
 @NgModule({
--- frontend/frontend/src/app/upload-file/upload-file.component.ts
+++ frontend/frontend/src/app/upload-file/upload-file.component.ts
@@ -1,17 +1,34 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+
+interface ApiResponse {
+  q0: string;
+  q1: string;
+  q2: string;
+  q3: string;
+  q4: string;
+  q5: string;
+  q6: string;
+  q7: string;
+  q8: string;
+  q9: string;
+}
 
 @Component({
   selector: 'app-upload-file',
   templateUrl: './upload-file.component.html',
   styleUrls: ['./upload-file.component.scss']
 })
+
 export class UploadFileComponent {
 
   fileName: string = '';
   selectedFile?: File;
+  isLoading = false;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   onFileSelected(event: any): void {
     this.selectedFile = event.target.files[0];
@@ -23,17 +40,21 @@ export class UploadFileComponent {
       return;
     }
 
+    this.isLoading = true;
+
     const formData = new FormData();
-    formData.append('pdfFile', this.selectedFile, this.selectedFile.name);
+    formData.append('files', this.selectedFile, this.selectedFile.name); // Use the correct parameter name
 
-    formData.append('fileName', this.fileName);
-
-    this.http.post('http://your-api-endpoint.com/upload', formData).subscribe(
+    this.http.post<ApiResponse[]>('http://127.0.0.1:8000/uploadfiles', formData).subscribe(
       (response) => {
         console.log(response);
+        localStorage.setItem('questions', JSON.stringify(response));
+        this.isLoading = false;
+        this.router.navigate(['/dashboard']);
       },
       (error) => {
         console.log(error);
+        this.isLoading = false;
       }
     );
   }
